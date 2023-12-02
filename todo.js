ef64f4a20e189eaec9d9ce623e9602fd4d2aad45
abraham hailebebriel <abraham>
2023-12-02 19:44:41 +0300

    todo list

---
+++ todo.js
@@ -0,0 +1,14 @@
+let addtodobutton = document.getElementById('addtodo');
+let todocontainer = document.getElementById("todocontainer");
+let inputfield = document.getElementById('inputfield');
+
+addtodobutton.addEventListener("click", function(){
+    var paragraph =document.createElement("p");
+    paragraph.classList.add("paragraph-styling");
+    paragraph.innerText = inputfield.value;
+    todocontainer.appendChild(paragraph)
+    inputfield.value = "";
+    paragraph.addEventListener("click", function(){
+       todocontainer.removeChild(paragraph)
+    })
+})
